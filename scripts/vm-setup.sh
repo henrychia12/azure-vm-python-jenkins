a2e9c51108c722d341353808028c28d728cc4f14
henrychia12 <henry_chia21@hotmail.co.uk>
2019-06-25 14:24:02 +0100

    vm-setup script complete

--- scripts/vm-setup.sh
+++ scripts/vm-setup.sh
@@ -12,7 +12,24 @@ az network vnet subnet create --resource-group JenkinsResource --vnet-name Jenki
 # create network security group
 az network nsg create --resource-group JenkinsResource --name JenkinsNetworkSecurityGroup
 
-# setting ssh rule on nsg
-az network nsg rule create --resource-group JenkinsResource --name ssh --priority 200 --nsg-name JenkinsNetworkSecuirtyGroup --destination-port-ranges 22
+# HTTP rule set on nsg
+az network nsg rule create --resource-group JenkinsResource --name HTTP --priority 300 --nsg-name JenkinsNetworkSecurityGroup
 
+# setting ssh rule on nsg
+az network nsg rule create --resource-group JenkinsResource --name SSH --priority 200 --nsg-name JenkinsNetworkSecuirtyGroup --destination-port-ranges 22
+
+# creating DNS and Static Public IP
+az network public-ip create --resource-group JenkinsResource --name JenkinsPublicIP --dns-name henrychia12 --allocation-method Static
+
+# creating Network Interface Name with nsg
+az network nic create --resource-group JenkinsResource --name JenkinsNetworkInterface --vnet-name JenkinsNetwork --subnet JenkinsSubnet --network-security-group JenkinsNetworkSecurityGroup
+
+# create jenkins host virtual machine
+az vm create --resource-group JenkinsResource --name JenkinsHostVM --IMAGE UbuntuLTS --nics JenkinsNetworkInterface 
+
+# create jenkins slave virtual machine
+az vm create --resource-group JenkinsResource --name JenkinsSlaveVM --IMAGE UbuntuLTS --nics JenkinsNetworkInterface
+
+# create python server virtual machine
+az vm create --resource-group JenkinsResource --name JenkinsPythonVM --IMAGE UbuntuLTS --nics JenkinsNetworkInterface
 
